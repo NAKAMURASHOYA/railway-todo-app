4e6e2c9f1c0946b58feedb5062c21270587b4069
Sho_N <shoya452511@gmail.com>
2025-12-24 00:29:36 +0900

    ライブラリのバージョンアップとESlint&Prettier導入

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,11 +1,11 @@
-import { useDispatch } from 'react-redux'
-import { useCallback } from 'react'
-import { useHistory } from 'react-router-dom'
-import { signup } from '~/store/auth'
+import { useDispatch } from 'react-redux';
+import { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom'; // useHistoryからuseNavigateに変更:React Router v6対応
+import { signup } from '~/store/auth';
 
 export const useSignup = () => {
-  const dispatch = useDispatch()
-  const history = useHistory()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleSignup = useCallback(
     async ({ email, password, name }) => {
@@ -15,13 +15,13 @@ export const useSignup = () => {
           password,
           name,
         }),
-      ).unwrap()
-      history.push('/')
+      ).unwrap();
+      navigate('/');
     },
-    [useDispatch],
-  )
+    [dispatch, navigate],
+  );
 
   return {
     signup: handleSignup,
-  }
-}
+  };
+};
